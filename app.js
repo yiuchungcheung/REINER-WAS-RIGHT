6a5c489ccd5db6e2b682e01f82522ff4f6a02625
victorgmaciel <vvv.maciel@gmail.com>
2022-02-18 16:02:21 -0800

    Express, React, mySQL code added.

--- app.js
+++ app.js
@@ -2,15 +2,22 @@ import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
-
+import mysql from 'mysql';
+import session from 'express-session';
 import indexRouter from './routes/index.js';
 import usersRouter from './routes/users.js';
-
+import loginRouter from './nodelogin/login.js';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+const connection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'MaposUm3C!',
+    database: 'nodelogin'
+});
 
 var app = express();
 
@@ -18,9 +25,42 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'static')));
+app.use('/homepage', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/', loginRouter)
+app.post('/auth', function (request, response) {
+    // Capture the input fields
+    let username = request.body.username;
+    let password = request.body.password;
+    // Ensure the input fields exists and are not empty
+    if (username && password) {
+        // Execute SQL query that'll select the account from the database based on the specified username and password
+        connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function (error, results, fields) {
+            // If there is an issue with the query, output the error
+            if (error) throw error;
+            // If the account exists
+            if (results.length > 0) {
+                // Authenticate the user
+                request.session.loggedin = true;
+                request.session.username = username;
+                // Redirect to home page
+                response.redirect('/home');
+            } else {
+                response.send('Incorrect Username and/or Password!');
+            }
+            response.end();
+        });
+    } else {
+        response.send('Please enter Username and Password!');
+        response.end();
+    }
+});
 export default app;
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,21 @@ import express from 'express';
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/homepage', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
 });
 
 export default router;
