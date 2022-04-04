48b47ef3decba60278204888e0cdd04364f39c96
Sam <samuelw2000@gmail.com>
2022-04-03 21:06:01 -0700

    import basic firebase functionalities

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { initializeApp } from 'firebase/app';
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  //...
+};
+
+const app = initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
