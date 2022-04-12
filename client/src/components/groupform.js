0473542eac9616284d148fa8a5e04877f769a1a3
Victor! <victor88@uw.edu>
2022-04-11 19:48:24 -0700

    basic create group works.

--- client/src/components/groupform.js
+++ client/src/components/groupform.js
@@ -1,24 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import firebase from '../firebase';
 
 export default function GroupForm() {
+    const groupRef = useRef();
     const [groups, setGroupName] = useState('');
 
     const handleOnChange = (e) => {
         setGroupName(e.target.value);
     };
     const createGroup = () => {
-      const groupRef = firebase.database().ref('groups');
+      const groupRef = firebase.database().ref('groups/' + groupRef);
       const group = {
-        groups,
+        groups: groups
       };
       groupRef.push(group)
     }
     
   return (
     <div>
-        <input type="text" onChange={handleOnChange}/>
-        <button onClick={createGroup}>Create Group</button>
+        <input type="text" onChange= {handleOnChange} ref={groupRef}/>
+        <button onClick= {createGroup} >Create Group!</button>
     </div>
   )
 }
--- client/src/pages/create.js
+++ client/src/pages/create.js
@@ -1,9 +1,22 @@
+import { getDatabase,ref, set } from 'firebase/database';
 import React, { useRef, useState } from 'react';
 import { Form, Button, Card, Alert } from 'react-bootstrap';
 import GroupForm from '../components/groupform'
 const link = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);;
 
 const Create = () => {
+    const groupRef = useRef();
+    const [group, setGroup] = useState("");
+
+    const handleGroupChange = (e) => {
+        setGroup(e.target.value)
+    }
+    function writeGroupData() {
+        const db = getDatabase();
+        set(ref(db, 'groups/'+ '3'), {
+            group: group
+        });
+    }
     return (
         <div>
             <h3>Create page</h3>
@@ -18,26 +31,26 @@ const Create = () => {
                             </div> */}
                             <label for="groupName" class="col-sm-2 col-form-label">Group Name</label>
                             <div class="col-sm-10">
-                                <GroupForm/>
+                                <GroupForm />
                             </div>
                         </div>
                     </li>
                     <li class="list-group-item reply">Create Group</li>
                 </ul>
-                
+
             </div>
             <Card>
-                    <Card.Body>
-                        <h2 className="text-center mb-4">Room Code: {link}</h2>
-                        <Form>
-                            <Form.Group id="groupName">
-                                <Form.Label>Group Name</Form.Label>
-                                <Form.Control type="text" placeholder="Group Name"  required />
-                            </Form.Group>
-                            <Button className="w-100 mt-4 outline" type="submit">Create</Button>
-                        </Form>
-                    </Card.Body>
-                </Card>
+                <Card.Body>
+                    <h2 className="text-center mb-4">Room Code: {link}</h2>
+                    <Form onSubmit={writeGroupData}>
+                        <Form.Group id="groupName">
+                            <Form.Label>Group Name</Form.Label>
+                            <Form.Control type="text" placeholder="Group Name" ref = {groupRef} onChange={ handleGroupChange} required />
+                        </Form.Group>
+                        <Button className="w-100 mt-4 outline" type="submit">Create</Button>
+                    </Form>
+                </Card.Body>
+            </Card>
         </div>
     );
 }
