137dde68c9fae24f31f52a560961c90339fa6d8c
yiu chung <59578307+yiuchungcheung@users.noreply.github.com>
2022-04-15 15:52:18 -0700

    updated navbar.js

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -17,15 +17,15 @@ const Nav = () => {
             
             <div class="container">
                 <nav class="navbar navbar-expand-lg navbar-light">
-                    <a class="navbar-brand" href="#/home">moment</a>
+                    <a class="navbar-brand" href="/home">moment</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarNav">
                         <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
                         <div id="nav-buttons">
-                        <a href="#/create" class="solid btn btn-primary btn-md mr-5"  role="button" aria-disabled="true">Create Group</a>
-                        <a href="#/join" class="solid btn btn-secondary btn-md green" role="button" aria-disabled="true">Join Group</a>
+                        <a href="/create" class="solid btn btn-primary btn-md mr-5"  role="button" aria-disabled="true">Create Group</a>
+                        <a href="/join" class="solid btn btn-secondary btn-md green" role="button" aria-disabled="true">Join Group</a>
                         <li class="nav-item"><a class="nav-link" onClick={logout}>Logout</a></li>
                         </div>
                         </ul>
