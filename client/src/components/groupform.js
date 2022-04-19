35031b63cdb1af8dc120c513eb6f2a2c5c812e4a
victorgmaciel <vvv.maciel@gmail.com>
2022-04-19 12:50:14 -0700

    working on creating group.

--- client/src/components/groupform.js
+++ client/src/components/groupform.js
@@ -2,24 +2,24 @@ import React, { useState, useRef } from 'react'
 import firebase from '../firebase';
 
 export default function GroupForm() {
-    const groupRef = useRef();
-    const [groups, setGroupName] = useState('');
+  const groupRef = useRef();
+  const [groups, setGroupName] = useState('');
 
-    const handleOnChange = (e) => {
-        setGroupName(e.target.value);
+  const handleOnChange = (e) => {
+    setGroupName(e.target.value);
+  };
+  const createGroup = () => {
+    const groupRef = firebase.database().ref('groups/' + groupRef);
+    const group = {
+      groups: groups
     };
-    const createGroup = () => {
-      const groupRef = firebase.database().ref('groups/' + groupRef);
-      const group = {
-        groups: groups
-      };
-      groupRef.push(group)
-    }
-    
+    groupRef.push(group)
+  }
+
   return (
     <div>
-        <input type="text" onChange= {handleOnChange} ref={groupRef}/>
-        <button onClick= {createGroup} >Create Group!</button>
+      <input type="text" onChange={handleOnChange} ref={groupRef} />
+      <button onClick={createGroup} >Create Group!</button>
     </div>
   )
 }
